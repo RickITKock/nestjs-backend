3fcc9f49c8b8aa5d31e412ee4893e00310afb25f
Rick Kock <rit.kock@gmail.com>
2024-04-07 20:44:41 +0200

    Ensure only a class can be used as parameter

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,15 +8,17 @@ import {
   Patch,
   Post,
   Query,
-  UseInterceptors,
 } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
-import { Serialize, SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import {
+  Serialize,
+} from 'src/interceptors/serialize.interceptor';
 import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
   constructor(private userService: UsersService) {}
 
@@ -26,7 +28,6 @@ export class UsersController {
     this.userService.createUser(email, password);
   }
 
-  @Serialize(UserDto)
   @Get('/:id')
   async findUser(@Param('id') id: string) {
     const user = await this.userService.findUser(parseInt(id));
