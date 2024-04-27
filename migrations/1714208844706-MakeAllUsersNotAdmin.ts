438052964ab6f3d19a231ce8af49531115794626
Rick Kock <rit.kock@gmail.com>
2024-04-27 12:11:47 +0200

    Add proper migration name

--- migrations/1714208844706-MakeAllUsersNotAdmin.ts
+++ migrations/1714208844706-MakeAllUsersNotAdmin.ts
@@ -1,8 +1,8 @@
 import { Logger } from '@nestjs/common';
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class UserRegistrationDate1714208844706 implements MigrationInterface {
-  private readonly logger = new Logger(UserRegistrationDate1714208844706.name);
+export class MakeAllUsersNotAdmin1714208844706 implements MigrationInterface {
+  private readonly logger = new Logger(MakeAllUsersNotAdmin1714208844706.name);
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     this.logger.log('Up');
